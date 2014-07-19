f6433dd9f6e0eb059c3dcc64592099cb61c6fda4
Hao Lian <me@haolian.org>
2014-07-19 14:01:09 -0400

    Use base-prelude and wreq; clean up text usages

--- Main.hs
+++ Main.hs
@@ -1,29 +1,28 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, NoImplicitPrelude #-}
 
-import Control.Concurrent.MVar (MVar)
+import           BasePrelude hiding (putStrLn)
+import           Control.Concurrent.MVar (MVar)
 import qualified Control.Concurrent.MVar as MVar
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Lazy as L
+import           Control.Lens hiding ((.=))
+import           Data.Aeson
+import           Data.Map (Map)
 import qualified Data.Map as M
-import Data.Aeson
-import Data.Functor ((<$>))
-import Data.Map (Map)
-import Data.Maybe (listToMaybe)
-import Data.Text.Lazy (Text)
-import Data.Text.Lazy.IO (putStrLn)
-import qualified Data.Text.Lazy as T
-import Data.Text.Lazy.Encoding (decodeUtf8, encodeUtf8)
+import           Data.Text (Text)
+import qualified Data.Text as T
+import           Data.Text.Encoding (decodeUtf8)
 import qualified Data.Text.Format as F
-import Network.Wai
-import qualified Network.HTTP.Conduit as H
-import Network.HTTP.Types (status200, status400)
-import Network.Wai.Handler.Warp (run)
-import Prelude hiding (putStrLn)
-import System.Directory (doesFileExist)
-import System.Environment (getArgs)
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TLE
+import           Data.Text.Lazy.IO (putStrLn)
+import           Network.HTTP.Types (status200, status400)
+import           Network.Wai
+import           Network.Wai.Handler.Warp (run)
+import           Network.Wreq (responseBody, post)
+import           System.Directory (doesFileExist)
+import           System.Environment (getArgs)
 
 newtype Channel = Channel Text deriving (Eq, Ord, Show)
-newtype User = User Text deriving (Eq, Ord)
+newtype User = User Text deriving (Eq, Ord, Show)
 newtype Emoji = Emoji Text
 data Want = WantsUser User | WantsChannel deriving (Eq, Show, Ord)
 data Person = Person User Channel Want deriving (Eq, Show, Ord)
@@ -31,15 +30,12 @@ data Notice = Notice Channel Text Emoji
 
 type DB = Map Channel [Person]
 
-instance Show User where
-  show (User s) = T.unpack s
-
 instance ToJSON Notice where
   toJSON (Notice (Channel channel) text (Emoji emoji)) =
-    object [ "channel" .= T.concat ["#", channel]
-           , "icon_emoji" .= T.concat [":", emoji, ":"]
-           , "parse" .= String "full"
-           , "username" .= String "hi5bot"
+    object [ "channel" .= ("#" <> channel)
+           , "icon_emoji" .= (":" <> emoji <> ":")
+           , "parse" .= ("full" :: Text)
+           , "username" .= ("hi5bot" :: Text)
            , "text" .= text
            ]
 
@@ -72,12 +68,12 @@ personOfRequest raw = do
     Just userName -> Person user channel (WantsUser $ userOf userName)
 
   where
-    textOf = decodeUtf8 . L.fromChunks . return
+    textOf = decodeUtf8
     params = M.fromList [(textOf k, textOf <$> v) | (k, v) <- queryString raw]
     p key = case M.lookup key params of
-      Just (Just "") -> Left (T.concat ["Empty key in params: ", key])
+      Just (Just "") -> Left ("Empty key in params: " <> key)
       Just (Just value) -> Right value
-      _  -> Left (T.concat ["Unable to find key in params: ", key])
+      _  -> Left ("Unable to find key in params: " <> key)
     text = case p "text" of Left _ -> Nothing; Right x -> Just x
     userOf = User . T.filter (/= '@')
     channelOf = Channel
@@ -92,35 +88,30 @@ noticeOfSecondHand (Person (User giver) _ _) (Person (User receiver) channel wan
               | giver /= user' -> F.format "swoops in for a high-five with @{}! (Caw caw! Better luck next time, @{}.) :hand::octopus:" (receiver, user')
               | otherwise -> F.format "high-fives @{}, and everybody's eyes widen with respect. :hand::guitar:" (F.Only receiver)
             WantsChannel -> F.format "high-fives @{}! :hand:" (F.Only receiver)
-        text = F.format "@{} {}" (giver, addendum)
+        text = TL.toStrict $ F.format "@{} {}" (giver, addendum)
 
 noticeOfFirstHand :: Person -> Notice
 noticeOfFirstHand (Person (User user) channel want) = Notice channel text (Emoji "hand")
   where addendum = case want of
           WantsUser (User user') -> F.format " for @{} to slap." (F.Only user')
           WantsChannel -> "."
-        text = F.format "@{} raises a hand high into the air{}" (user, addendum)
+        text = TL.toStrict $ F.format "@{} raises a hand high into the air{}" (user, addendum)
 
 postPayload :: String -> Notice -> IO ()
 postPayload token notice = do
   args <- getArgs
   case args == ["-n"] of
     True -> do
-      putStrLn (T.concat ["+ Pretending (-n) to post ", (decodeUtf8 . encode) notice])
+      putStrLn $ F.format "+ Pretending (-n) to post {}" [(TLE.decodeUtf8 . encode) notice]
     False -> do
-      response <- get request
-      putStrLn (decodeUtf8 (H.responseBody response))
+      r <- post url (encode notice)
+      putStrLn $ TLE.decodeUtf8 (r ^. responseBody)
     where
-      baseRequest = H.parseUrl ("https://trello.slack.com/services/hooks/incoming-webhook?token=" ++ token)
-      request = fmap (H.urlEncodedBody [("payload", bytes)]) baseRequest
-      get r = r >>= (H.withManager . H.httpLbs)
-      bytes = (B.concat . L.toChunks . encode) notice
+      url = "https://trello.slack.com/services/hooks/incoming-webhook?token=" <> token
 
 application :: MVar DB -> Application
 application dbM rawRequest respond = do
-  putStrLn $ T.concat ["+ Incoming request: "
-                      , decodeUtf8 $ L.fromChunks [rawPathInfo rawRequest]
-                      , decodeUtf8 $ L.fromChunks [rawQueryString rawRequest]]
+  putStrLn $ F.format "+ Incoming request: {} {}" (map decodeUtf8 [rawPathInfo rawRequest, rawQueryString rawRequest])
   db <- MVar.readMVar dbM
   case personOfRequest rawRequest of
     Left err -> respondWithError err
@@ -133,7 +124,7 @@ application dbM rawRequest respond = do
     postWant receiver = tokenM >>= \t -> postPayload t (noticeOfFirstHand receiver)
     postGive giver receiver = tokenM >>= \t -> postPayload t (noticeOfSecondHand giver receiver)
     respondWithEmpty = (respond . responseLBS status200 headers) ""
-    respondWithError = respond . responseLBS status400 headers . encodeUtf8
+    respondWithError = respond . responseLBS status400 headers . TLE.encodeUtf8 . TL.fromStrict
 
 main :: IO ()
 main = do
